714aec4a066f2b74dfde410622e78e55e38107e6
mariusbrandorff <marius.brandorffhansen@gmail.com>
2025-11-08 14:17:27 +0100

    Added expose to backend dependencies

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -3,6 +3,7 @@ val koin_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val postgres_version: String by project
+val exposed: String by project
 
 plugins {
     kotlin("jvm") version "2.2.20"
@@ -32,6 +33,10 @@ dependencies {
     implementation("io.ktor:ktor-server-netty")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml")
+    implementation("org.jetbrains.exposed:exposed-core:$exposed")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed")
+    implementation("com.h2database:h2:$h2_version")
+
     testImplementation("io.ktor:ktor-server-test-host")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }

1eb8689b40b3057dcc3e36f070c0e8251adcc831
Rolf <144017838+RolfErFloor@users.noreply.github.com>
2025-11-13 09:30:21 +0100

    Initial show groups screen

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
     implementation("androidx.compose.material:material-icons-extended:1.3.0")
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.compose.foundation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- frontend/app/src/main/java/com/example/japp/screens/CreateGroupScreen.kt
+++ frontend/app/src/main/java/com/example/japp/screens/CreateGroupScreen.kt
@@ -1,10 +1,13 @@
 package com.example.japp.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,57 +27,63 @@ import com.example.japp.composables.GroupIcon
 fun CreateGroupScreen(navController: NavController? = null) {
     var name by remember { mutableStateOf("") }
     var descript by remember { mutableStateOf("") }
-    var nameValid by remember { mutableStateOf(false) }
-    var descriptValid by remember { mutableStateOf(false) }
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        Alignment.Center
-    ){
-        Column (
+    var nameValid by remember { mutableStateOf(true) }
+    var descriptValid by remember { mutableStateOf(true) }
+        Column(
+            modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(20.dp)
-        ){
+            verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.CenterVertically)
+        ) {
             GroupIcon(name)
-            OutlinedTextField(
-                name,
-                isError = !nameValid,
-                onValueChange = {
-                    name = it
-                    if (it.length > 50){
-                      nameValid = false
-                    }
-                    else nameValid = true
-                },
-                label = { Text("Group name") },
-                supportingText = {
-                    if (!nameValid){
-                        Text("Name must be less than 50 characters.")
-                    }
-                }
-            )
-            OutlinedTextField(
-                descript,
-                isError = !descriptValid,
-                onValueChange = {
-                    descript = it
-                    if (it.length > 150){
-                        descriptValid = false
-                    }
-                    else descriptValid = true
-                    },
-                label = { Text("Description") },
-                supportingText = {
-                    if (!descriptValid){
-                        Text("Description must be less than 150 characters")
-                    }
-                }
-            )
-            Button(onClick = {}) {
-                Text("Submit")
-                //TODO add endpoints 
-            }
+            Box(
+                Modifier.background(MaterialTheme.colorScheme.surfaceContainer).padding(15.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(20.dp)
+                ) {
 
-        }
+                    OutlinedTextField(
+                        name,
+                        isError = !nameValid,
+                        onValueChange = {
+                            name = it
+                            if (it.length > 50 || it.isEmpty()) {
+                                nameValid = false
+                            } else nameValid = true
+                        },
+                        label = { Text("Group name") },
+                        supportingText = {
+                            if (!nameValid) {
+                                Text("Name must not be empty and be less than 50 characters.")
+                            }
+                        }
+                    )
+                    OutlinedTextField(
+                        descript,
+                        isError = !descriptValid,
+                        onValueChange = {
+                            descript = it
+                            if (it.length > 500 || it.isEmpty()) {
+                                descriptValid = false
+                            } else descriptValid = true
+                        },
+                        label = { Text("Description") },
+                        supportingText = {
+                            if (!descriptValid) {
+                                Text("Description must not be empty and be less than 500 characters")
+                            }
+                        }
+                    )
+                    Button(onClick = {}) {
+                        Text("Submit")
+                        //TODO add endpoints
+                    }
+
+                }
+
+            }
 
     }
 }

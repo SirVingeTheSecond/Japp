14a314b778aacb76370138a916d1c5a50f326df7
Mikkel Søe Nielsen <mikni23@student.sdu.dk>
2025-11-13 17:34:23 +0100

    time printer

--- frontend/app/src/main/java/com/example/japp/screens/ActivityScreen.kt
+++ frontend/app/src/main/java/com/example/japp/screens/ActivityScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFrom
@@ -22,6 +23,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,38 +31,63 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.max
 import androidx.navigation.NavController
 import com.example.japp.AppDestinations
-@Preview
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+
+
+fun printableDatetime(time: LocalDateTime): String {
+
+    val minutes = ChronoUnit.MINUTES.between(time, LocalDateTime.now())
+    val weeks = ChronoUnit.WEEKS.between(time, LocalDateTime.now())
+    val years = ChronoUnit.YEARS.between(time, LocalDateTime.now())
+
+    if (minutes < 1) {
+        return "now"
+    } else if (years == 0L && time.dayOfYear == LocalDateTime.now().dayOfYear) {
+        return "today"
+    } else if (years == 0L && time.dayOfYear == (LocalDateTime.now().dayOfYear-1)) {
+        return "yesterday"
+    } else if (years == 0L && weeks == 0L) {
+        return time.dayOfWeek.name
+    } else {
+        val dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yy")
+        return time.format(dateTimeFormatter)
+    }
+}
+
 @Composable
-fun ActivityScreen() {
-    Scaffold (modifier = Modifier
-        .background(MaterialTheme.colorScheme.primary))
-    { paddingValues ->
-        Row (
+fun ActivityRow(
+    icon: ImageVector,
+    user: String,
+    action: String,
+    date: LocalDateTime
+) {
+    return Row (
             modifier = Modifier
-                .padding(paddingValues)
-                .padding(16.dp),
+                .padding(top = 10.dp),
             horizontalArrangement = Arrangement.spacedBy(4.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
             Icon(
-                imageVector = Icons.Default.Circle,
-                contentDescription = "Activity",
+                imageVector = icon,
+                contentDescription = "Activity type",
                 tint = MaterialTheme.colorScheme.primary,
                 modifier = Modifier
                     .size(24.dp)
             )
 
             Text(
-                text = "Mikkel",
-                style = MaterialTheme.typography.bodyMedium,
+                text = user,
+                style = MaterialTheme.typography.bodySmall,
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis,
                 fontWeight = FontWeight.Bold
             )
 
             Text(
-                text = "added a receipt for 10$ to test group",
-                style = MaterialTheme.typography.bodyMedium,
+                text = action,
+                style = MaterialTheme.typography.bodySmall,
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis
             )
@@ -68,9 +95,29 @@ fun ActivityScreen() {
             Spacer(modifier = Modifier.weight(1f))
 
             Text(
-                text = "yesterday",
-                style = MaterialTheme.typography.bodySmall
+                text = printableDatetime(date),
+                style = MaterialTheme.typography.labelSmall
             )
+        }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun ActivityScreen() {
+    Scaffold (
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.primary)
+    ) { paddingValues -> Column (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(paddingValues)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ActivityRow(icon = Icons.Default.Circle, user = "Mikkel", action = "added a receipt for 10$ to test group", date = LocalDateTime.now() )
+            ActivityRow(icon = Icons.Default.Circle, user = "Mikkel", action = "added a receipt for 20$ to test group", date = LocalDateTime.of(2025,11,12,12,0) )
+            ActivityRow(icon = Icons.Default.Circle, user = "Mikkel", action = "added a receipt for 30$ to test group", date = LocalDateTime.of(2025, 10, 1, 12, 0) )
         }
 
     }

384eeb8e18f80237b8c1ef9b7db6f4db3e7b2744
PausedMagician <75096283+PausedMagician@users.noreply.github.com>
2025-11-13 11:31:00 +0100

    Initial setup

--- frontend/app/src/main/java/com/example/japp/screens/HomeScreen.kt
+++ frontend/app/src/main/java/com/example/japp/screens/HomeScreen.kt
@@ -1,14 +1,65 @@
 package com.example.japp.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.japp.AppDestinations
 
+@Preview(showSystemUi = true)
 @Composable
-fun HomeScreen(navController: NavController) {
-    Button(onClick = { navController.navigate(AppDestinations.SCAN.route) }) {
-        Text("HELLO (Go to qr?)")
+fun HomeScreen(navController: NavController? = null) {
+    Column(
+        Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        QuickStats()
+        Button(onClick = { navController?.navigate(AppDestinations.SCAN.route) }) {
+            Text("HELLO (Go to qr?)")
+        }
+    }
+}
+
+
+@Composable
+fun QuickStats() {
+    Row (
+        Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        Pill(label = "Owed", color = MaterialTheme.colorScheme.primaryContainer, textColor = MaterialTheme.colorScheme.onPrimaryContainer)
+        Pill(label = "Ratio", color = MaterialTheme.colorScheme.tertiaryContainer, textColor = MaterialTheme.colorScheme.onTertiaryContainer)
+        Pill(label = "Owes", color = MaterialTheme.colorScheme.secondaryContainer, textColor = MaterialTheme.colorScheme.onSecondaryContainer)
+    }
+}
+
+@Composable
+fun Pill(content: String = "Idk?", label: String? = null, color: Color? = null, textColor: Color? = null) {
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        label?.let { Text(it) }
+        Box (
+            Modifier.clip(RoundedCornerShape(100.dp)).background(color ?: MaterialTheme.colorScheme.primaryContainer).padding(vertical = 6.dp, horizontal = 12.dp)
+        ) {
+            Text(content, color = textColor ?: MaterialTheme.colorScheme.onPrimaryContainer)
+        }
     }
 }

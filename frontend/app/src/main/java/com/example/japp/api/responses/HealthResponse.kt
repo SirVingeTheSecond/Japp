ef43d23e0897e34d57f857049c57a399b5997c69
KongKrog1 <144017664+KongKrog1@users.noreply.github.com>
2025-11-17 22:03:33 +0100

    Frontend/update namespace (#67)
    update namespace to not include .example

--- frontend/app/src/main/java/com/example/japp/api/responses/HealthResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.japp.api.responses
-
-import com.google.gson.annotations.SerializedName
-
-data class HealthResponse(
-    val status: String,
-    val version: String,
-    val timestamp: Long,
-)

58740f815f90b519f3ac679a906293eaf1b6ca9f
Rolf <144017838+RolfErFloor@users.noreply.github.com>
2025-11-18 12:02:31 +0100

    merge main into branch

--- frontend/app/src/main/java/com/japp/composables/GroupIcon.kt
+++ frontend/app/src/main/java/com/japp/composables/GroupIcon.kt
@@ -1,11 +1,10 @@
-package com.example.japp.composables
+package com.japp.composables
 
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -17,11 +16,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.japp.ui.theme.JappTheme
-import java.util.Locale
 
 @Composable
 fun GroupIcon(content: String, modifier: Modifier = Modifier){
--- frontend/app/src/main/java/com/japp/screens/CreateGroupScreen.kt
+++ frontend/app/src/main/java/com/japp/screens/CreateGroupScreen.kt
@@ -1,6 +1,15 @@
 package com.example.japp.screens
 
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -13,6 +22,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.japp.AppDestinations
+import com.japp.api.RetrofitClient
+import com.japp.api.responses.group.CreateGroupRequest
+import com.japp.api.responses.group.GroupDto
+import com.japp.composables.GroupIcon
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 @Preview(showBackground = true, showSystemUi = true)
